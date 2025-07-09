65f497e98f7ae238f05767918739be716258d3f4
Celso <celsodiehl@gmail.com>
2025-07-09 15:45:07 -0300

    Criado Classe e bd Product

---
+++ src/main/java/com/crudtool/bestcrud/BestcrudApplication.java
@@ -0,0 +1,13 @@
+package com.crudtool.bestcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestcrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/crudtool/bestcrud/models/Product.java
@@ -0,0 +1,30 @@
+package com.crudtool.bestcrud.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "products")
+@Setter
+@Getter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String brand;
+    private String category;
+    private String price;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Date createdAt;
+    private String imageFileName;
+
+
+}
